ec74a14b6f38acd7fa5620046e2c8cce1aa2529b
jet-glitch <jetriddle@gmail.com>
2026-04-28 14:56:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000,
+  },
+  esbuild: {
+    target: 'es2020'
+  }
+})
